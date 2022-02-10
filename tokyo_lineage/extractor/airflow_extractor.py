b20d1a61651db91e64354d11b4fd634311437bf0
dion-ricky <dionrickysptr@gmail.com>
2022-02-10 00:56:18 +0000

    Created blueprint of AirflowExtractor

--- tokyo_lineage/extractor/airflow_extractor.py
+++ tokyo_lineage/extractor/airflow_extractor.py
@@ -1,9 +1,13 @@
 from airflow.models.dagrun import DagRun
 
-from extractor.base_extractor import BaseExtractor
+from airflow.models import BaseOperator
+from airflow.models.taskinstance import TaskInstance
 
-from utils.airflow import get_dagbag, get_task_instances_from_dagrun, \
-    get_dag_from_dagbag, get_task_from_dag, instantiate_task_from_ti
+from tokyo_lineage.extractor.base_extractor import BaseExtractor
+from tokyo_lineage.models.airflow_task import AirflowTask
+from tokyo_lineage.utils.airflow import get_dagbag, \
+    get_task_instances_from_dagrun, get_dag_from_dagbag, get_task_from_dag, \
+    instantiate_task_from_ti
 
 class AirflowExtractor(BaseExtractor):
     def __init__(self):
@@ -18,18 +22,24 @@ class AirflowExtractor(BaseExtractor):
             return
 
         for task_instance in task_instances:
-            _task = get_task_from_dag(dag, task_instances.task_id)
+            _task = get_task_from_dag(dag, task_instance.task_id)
             _task, _ = instantiate_task_from_ti(_task, task_instance)
 
             self._handle_task_run(_task, task_instance)
     
-    def _handle_task_run(self, task, task_instance):
-        # new models.airflow_task
-        # handle_task_run
-        pass
+    def _handle_task_run(
+        self,
+        task: BaseOperator,
+        task_instance: TaskInstance
+    ):
+        _task = AirflowTask(task.task_id, task, task_instance)
+        self.handle_task_run(_task)
 
-    def handle_task_run(self, task):
+    def handle_task_run(self, task: AirflowTask):
         # extract metadata
         # register start_task
         # register finish_task or fail_task
+        pass
+
+    def report_task(self, task: AirflowTask):
         pass

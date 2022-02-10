674791008f404e0e734130161b66b84cd54b55e2
dion-ricky <dionrickysptr@gmail.com>
2022-02-10 00:55:56 +0000

    Created AirflowTask class

---
+++ tokyo_lineage/models/airflow_task.py
@@ -0,0 +1,23 @@
+import attr
+
+from airflow.models import BaseOperator
+from airflow.models.taskinstance import TaskInstance
+
+from tokyo_lineage.models.base_task import BaseTask
+
+class AirflowTaskMismatch(Exception):
+    pass
+
+@attr.s
+class AirflowTask(BaseTask):
+    task: BaseOperator = attr.ib(init=True, default=None)
+    task_instance: TaskInstance = attr.ib(init=True, default=None)
+
+    @task_instance.validator
+    def _check_task_instance(self, attribute, value):
+        try:
+            assert (self.task.task_id == value.task_id) and \
+                    (value.task_id == self.task_id)
+        except:
+            raise AirflowTaskMismatch("Operator and TaskInstance task_id should match."\
+                    "{} != {}".format(self.task.task_id, value.task_id))

442c11afad1f722db9fb05cfe9705f28be66efde
dion-ricky <dionrickysptr@gmail.com>
2022-02-10 03:00:41 +0000

    Task model don't need to save task state

--- tokyo_lineage/models/base.py
+++ tokyo_lineage/models/base.py
@@ -1,13 +1,9 @@
 import attr
 
-from tokyo_lineage.const.state import TaskState
-
 @attr.s
 class BaseTask:
     task_id: str = attr.ib(init=True, default=None)
     operator: str = attr.ib(init=True, default=None)
-    state: TaskState = attr.ib(init=False, default=None, \
-                                validator=attr.validators.in_(TaskState))
 
 
 @attr.s

c582fb2620eb248f0b74045d65af5af505ff0d21
dion-ricky <dionrickysptr@gmail.com>
2022-03-02 06:49:06 +0000

    Test PostgresToAvro meta extractor #10

---
+++ tests/test_pg_to_avro_meta_extractor.py
@@ -0,0 +1,79 @@
+import getpass
+import platform
+import unittest
+from datetime import datetime
+from unittest.mock import Mock
+
+from airflow.utils.state import State
+
+from openlineage.common.dataset import Source
+
+from tokyo_lineage.models.airflow_task import AirflowTask
+from tokyo_lineage.metadata_extractor.airflow.postgres_to_avro_extractor \
+    import PostgresToAvroExtractor
+
+class TestPgToAvroMetaExtractor(unittest.TestCase):
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName)
+        task_id = 'test_task'
+        operator = 'test_operator'
+
+        _task = Mock()
+        _task.task_id = task_id
+        _task.dag_id = 'test_dag'
+
+        task_instance = Mock()
+        task_instance.state = State.SUCCESS
+        task_instance.task_id = task_id
+        task_instance.operator = operator
+        task_instance.try_number = 1
+        task_instance.execution_date = datetime(2022, 2, 10, 7, 0, 0)
+        task_instance.start_date = datetime(2022, 2, 10, 7, 0, 0)
+        task_instance.end_date = datetime(2022, 2, 10, 8, 0, 0)
+
+        task = AirflowTask(task_id, operator, _task, task_instance)
+
+        meta_extractor = PostgresToAvroExtractor(task)
+
+        self.task = task
+        self.task_instance = task_instance
+        self.meta_extractor = meta_extractor
+
+    def test_fs_scheme(self):
+        meta_extractor = self.meta_extractor
+        
+        self.assertEqual(meta_extractor._get_fs_scheme(), 'files')
+    
+    def test_fs_connection_uri(self):
+        meta_extractor = self.meta_extractor
+
+        self.assertEqual(meta_extractor._get_fs_connection_uri(), platform.uname().node)
+
+    def test_fs_authority(self):
+        meta_extractor = self.meta_extractor
+
+        user = getpass.getuser()
+        node = platform.uname().node
+
+        self.assertEqual(meta_extractor._get_fs_authority(), ':'.join([user, node]))
+
+    def test_fs_name(self):
+        meta_extractor = self.meta_extractor        
+
+        self.assertEqual(meta_extractor._get_fs_name(), 'test_dag_temp_fs')
+    
+    def test_fs_source(self):
+        meta_extractor = self.meta_extractor        
+        
+        fs_source = Source(
+            scheme=meta_extractor._get_fs_scheme(),
+            authority=meta_extractor._get_fs_authority(),
+            connection_url=meta_extractor._get_fs_connection_uri()
+        )
+
+        user = getpass.getuser()
+        node = platform.uname().node
+
+        self.assertEqual(fs_source.scheme, 'files')
+        self.assertEqual(fs_source.authority, ':'.join([user, node]))
+        self.assertEqual(fs_source.connection_url, node)

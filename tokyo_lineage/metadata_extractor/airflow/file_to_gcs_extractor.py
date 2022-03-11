daa46cd5d8a7141e5d5684100c93724a2e585a3f
dion-ricky <dionrickysptr@gmail.com>
2022-03-11 09:31:10 +0000

    Create FileToGCS meta extractor #15

---
+++ tokyo_lineage/metadata_extractor/airflow/file_to_gcs_extractor.py
@@ -0,0 +1,105 @@
+import platform
+from urllib.parse import urlparse
+from typing import Type, List, Optional
+
+from airflow.models import BaseOperator
+
+from openlineage.airflow.extractors.base import TaskMetadata
+from openlineage.common.dataset import Source, Dataset, Field
+
+from tokyo_lineage.models.base import BaseTask
+from tokyo_lineage.metadata_extractor.base import BaseMetadataExtractor
+
+from tokyo_lineage.utils.airflow import get_connection
+
+EXPORTER_OPERATOR_CLASSNAMES = ["PostgresToAvroOperator", "PostgresToJsonOperator"]
+
+class LocalFileToGcsExtractor(BaseMetadataExtractor):
+    def __init__(self, task: Type[BaseTask]):
+        super().__init__(task)
+
+    @classmethod
+    def get_operator_classnames(cls) -> List[str]:
+        return ["FileToGoogleCloudStorageOperator"]
+    
+    @property
+    def operator(self) -> Type[BaseOperator]:
+        return self.task.task
+
+    def extract(self) -> Optional[TaskMetadata]:
+        filesystem_source = Source(
+            scheme=self._get_fs_scheme(),
+            authority=self._get_fs_authority(),
+            connection_url=self._get_fs_connection_uri()
+        )
+
+        inputs = [
+            Dataset(
+                name=self._get_input_dataset_name(),
+                source=filesystem_source
+            )
+        ]
+
+        output_gcs_source = Source(
+            scheme=self._get_gcs_scheme(),
+            authority=self._get_gcs_authority(),
+            connection_url=self._get_gcs_connection_uri()
+        )
+
+        outputs = [
+            Dataset(
+                name=self.operator.bucket,
+                source=output_gcs_source
+            )
+        ]
+
+        return TaskMetadata(
+            name=f"{self.operator.dag_id}.{self.operator.task_id}",
+            inputs=[ds.to_openlineage_dataset() for ds in inputs],
+            outputs=[ds.to_openlineage_dataset() for ds in outputs]
+        )
+
+
+    def _get_gcs_connection(self):
+        conn = get_connection(self.operator.google_cloud_storage_conn_id)
+        return conn
+
+    def _get_gcs_scheme(self) -> str:
+        return 'gs'
+    
+    def _get_gcs_connection_uri(self) -> str:
+        conn = self._get_gcs_connection()
+        return conn.get_uri()
+
+    def _get_gcs_authority(self) -> str:
+        conn = self._get_gcs_connection()
+
+        if conn.host and conn.port:
+            return f'{conn.host}:{conn.port}'
+        else:
+            parsed = urlparse(conn.get_uri())
+            return f'{parsed.hostname}:{parsed.port}'
+
+    def _get_fs_scheme(self) -> str:
+        return 'file'
+
+    def _get_fs_connection_uri(self) -> str:
+        scheme = self._get_fs_scheme()
+        host = platform.uname().node
+        return f'{scheme}://{host}'
+
+    def _get_fs_authority(self) -> str:
+        return platform.uname().node
+
+    def _get_input_dataset_name(self) -> str:
+        exporter = self._get_nearest_exporter_upstream()
+        return '_'.join([exporter.dag_id, exporter.task_id])
+
+    def _get_nearest_exporter_upstream(self) -> Type[BaseOperator]:
+        operator = self.operator
+        
+        upstream_operators: List[BaseOperator] = operator.upstream_list[::-1]
+
+        for operator in upstream_operators:
+            if operator.__class__.__name__ in EXPORTER_OPERATOR_CLASSNAMES:
+                return operator

662724e4ee09686e959b0e73a86c8f3af2daab2b
dion-ricky <dionrickysptr@gmail.com>
2022-04-21 11:42:04 +0700

    Added more logging for troubleshooting #58

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from setuptools import find_namespace_packages
 
-VERSION = '0.5a5'
+VERSION = '0.5a6'
 
 setup(
     name="tokyo-lineage",
--- tokyo_lineage/metadata_extractor/airflow/postgres_to_avro_extractor.py
+++ tokyo_lineage/metadata_extractor/airflow/postgres_to_avro_extractor.py
@@ -94,12 +94,15 @@ class PostgresToAvroExtractor(BaseMetadataExtractor):
         ]
 
         # Extracting annotation from source
+        self.log.info("Extracting annotation from source input")
         if hasattr(self.operator, 'tokyolineage_params'):
+            self.log.info("Task has tokyolineage_params")
             try:
                 if self.operator.tokyolineage_params['is_annotation_available']:
+                    self.log.info("Annotation is available, trying to extract")
                     self._extract_annotations(inputs)
             except KeyError as e:
-                pass
+                self.log.error(e)
 
         filesystem_source = Source(
             scheme=self._get_fs_scheme(),

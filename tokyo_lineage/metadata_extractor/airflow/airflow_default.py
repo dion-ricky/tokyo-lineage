7ecb7f2a5dbb619b82b41580ef0ad5921ae02ace
dion-ricky <dionrickysptr@gmail.com>
2022-05-18 08:10:13 +0000

    Added MySqlToAvro to default airflow meta extractor

--- tokyo_lineage/metadata_extractor/airflow/airflow_default.py
+++ tokyo_lineage/metadata_extractor/airflow/airflow_default.py
@@ -1,3 +1,15 @@
-from tokyo_lineage.metadata_extractor.airflow import PostgresExtractor, PostgresToAvroExtractor, FileToGcsExtractor, GcsToBigQueryExtractor
+from tokyo_lineage.metadata_extractor.airflow import (
+    PostgresExtractor,
+    PostgresToAvroExtractor,
+    FileToGcsExtractor,
+    GcsToBigQueryExtractor,
+    MySqlToAvroExtractor
+)
 
-AIRFLOW_METADATA_EXTRACTORS = [PostgresExtractor, PostgresToAvroExtractor, FileToGcsExtractor, GcsToBigQueryExtractor]
+AIRFLOW_METADATA_EXTRACTORS = [
+    PostgresExtractor,
+    PostgresToAvroExtractor,
+    FileToGcsExtractor,
+    GcsToBigQueryExtractor,
+    MySqlToAvroExtractor
+]
